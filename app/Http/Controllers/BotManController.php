3b5f75cd1cdb64aaee446cb0cde8a9f3e83c8c05
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-10-30 23:39:35 +0300

    modified handle conversation

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -11,6 +11,10 @@ public function handle()
 {
     $botman = app('botman');
 
+    $botman->hears('start', function (BotMan $bot) {
+        $bot->startConversation(new ExampleConversation());
+    });
+
     $botman->listen();
 }
 
